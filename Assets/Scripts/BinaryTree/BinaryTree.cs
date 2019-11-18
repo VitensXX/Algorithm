a455de00da1018871e12125d4e2009b95e8827dd
VitensXX <vitens@foxmail.com>
2019-11-18 20:52:06 +0800

    BinaryTree

---
+++ Assets/Scripts/BinaryTree/BinaryTree.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vitens.BinaryTree
+{
+    public class BinaryTree
+    {
+        public BinaryTreeNode<int> head;
+
+        public BinaryTree(int[] nodes)
+        {
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                //构造一个节点
+                BinaryTreeNode<int> temp = new BinaryTreeNode<int>(nodes[i]);
+
+                //如果是第一个,作为根节点
+                if (i == 0)
+                {
+                    head = temp;
+                }
+
+
+
+                
+            }
+        }
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/BinaryTree/BinaryTreeNode.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vitens.BinaryTree
+{
+    public class BinaryTreeNode<T>
+    {
+        private T _data;
+        private BinaryTreeNode<T> _leftChildNode;
+        private BinaryTreeNode<T> _rightChildNode;
+
+        public T Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        public BinaryTreeNode<T> LeftChildNode
+        {
+            get { return _leftChildNode; }
+            set { _leftChildNode = value; }
+        }
+
+        public BinaryTreeNode<T> RightChildNode
+        {
+            get { return _rightChildNode; }
+            set { _rightChildNode = value; }
+        }
+
+        public BinaryTreeNode()
+        {
+            CreateTreeNode(default, null, null);
+        }
+
+        public BinaryTreeNode(T var)
+        {
+            CreateTreeNode(var, null, null);
+        }
+
+        public BinaryTreeNode(T var, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        {
+            CreateTreeNode(var, left, right);
+        }
+
+        //构造一个节点
+        void CreateTreeNode(T val, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        {
+            _data = val;
+            _leftChildNode = null;
+            _rightChildNode = null;
+        }
+    }
+}
