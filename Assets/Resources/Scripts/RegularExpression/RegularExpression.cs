c07d72cd7265c9bbab6c1663cac3c8e5491816d8
VitensXX <vitens@foxmail.com>
2019-12-24 19:04:13 +0800

    regular expression

--- Assets/Resources/Scripts/RegularExpression/RegularExpression.cs
+++ Assets/Resources/Scripts/RegularExpression/RegularExpression.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class RegularExpression
 {
+    public string input;
+    public string pattern;
+
     public static void PreciseCompare()
     {
         string input = "abs";
@@ -17,9 +20,23 @@ public class RegularExpression
     //是否为电话号码判断
     public static bool IsValidPhoneNum(string input)
     {
-        string pattern = "1\\d{10}";
+        string pattern = "^[1]+[3,5]+\\d{9}$";// "1\\d{10}";
 
         return Regex.IsMatch(input, pattern);
     }
+
+
+    public static void Compare()
+    {
+        //Debug.LogError(Regex.IsMatch("007", "00\\d"));//true
+        //Debug.LogError(Regex.IsMatch("0078", "00\\d\\d"));//true
+        Debug.LogError(Regex.IsMatch("1234", "\\d{1,5}"));//true 1~5个
+        //Debug.LogError(Regex.IsMatch("1234", "\\d{3,}"));//true 至少3个  逗号后面不能有空格
+        Debug.LogError(Regex.IsMatch("123456", "\\d{0,5}"));//true 至多5个
+        Debug.LogError(Regex.IsMatch("test_123", "\\w{0,3}"));//true \w  word表示字符(字母数字下划线)
+
+
+    }
+
 
 }
--- Assets/Resources/Scripts/Test.cs
+++ Assets/Resources/Scripts/Test.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public string input;
+    public string pattern;
+
     public void test()
     {
-        RegularExpression.PreciseCompare();
+        //RegularExpression.PreciseCompare();
+
+        RegularExpression.Compare();
+        //Debug.LogError(input  + " "+pattern);
+        //Debug.LogError("cur "+System.Text.RegularExpressions.Regex.IsMatch(input, pattern));
+        Debug.LogError("phonenum "+RegularExpression.IsValidPhoneNum("135456789111"));
     }
 }
