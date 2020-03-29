294f2121539c7e5f77fb54d1a56cba4691311fdf
VitensXX <vitens@foxmail.com>
2020-03-29 23:29:19 +0800

    “single linked list

---
+++ Assets/Resources/Scripts/LinkedList/LikedListTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LikedListTest : MonoBehaviour
+{
+    SingleLinkedList<string> _singleList;
+
+    private void Start()
+    {
+        _singleList = new SingleLinkedList<string>();
+        _singleList.head = new SingleLinkedList<string>.Node();
+        _singleList.head.next = null;
+    }
+
+    private string insertValuse = "0";
+    private string GetAtIndex = "0";
+    //private string textFieldString = "0";
+    private void OnGUI()
+    {
+        insertValuse = GUI.TextField(new Rect(110, 40, 80, 50), insertValuse);
+        if (GUI.Button(new Rect(20, 40, 80, 50), "头插"))
+        {
+            _singleList.InsertAtHead(insertValuse);
+            //Debug.LogError(textFieldString);
+        }
+
+        GetAtIndex = GUI.TextField(new Rect(110, 140, 80, 50), GetAtIndex);
+        if (GUI.Button(new Rect(20, 140, 80, 50), "获取"))
+        {
+            _singleList.GetAt(int.Parse(GetAtIndex));
+        }
+
+
+        if (GUI.Button(new Rect(20, 540, 80, 50), "打印"))
+        {
+            _singleList.DisplayList();
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/LinkedList/SingleLinkedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class SingleLinkedList<T>
+{
+    public class Node
+    {
+        public T value;
+        public Node next;
+    }
+
+    public Node head;
+
+    /// <summary>
+    /// 获取指定位置的元素
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public T GetAt(int index)
+    {
+        return GetNodeAt(index).value;
+    }
+
+    Node GetNodeAt(int index)
+    {
+        Node cur = head.next;
+        int i = 0;
+        while (cur != null && i < index)
+        {
+            cur = cur.next;
+            i++;
+        }
+
+        if (i == index)
+        {
+            Debug.Log("Get value:" + cur.value.ToString());
+            return cur;
+        }
+        else
+        {
+            throw new Exception("index is lager than list length!");
+        }
+    }
+
+    public bool InsertAtHead(T value)
+    {
+        Node n = new Node();
+        n.value = value;
+        n.next = head.next;
+        head.next = n;
+        Debug.LogError("insert "+value.ToString()+" at head success!");
+        return true;
+    }
+
+    public bool InsertAt(T node, int i)
+    {
+        //找到指定位置的前后节点
+        Node prevNode = GetNodeAt(i);
+        Node nextNode = prevNode.next;
+
+        //创建新节点 插入操作
+        Node newNode = new Node();
+        newNode.next = nextNode;
+        prevNode.next = newNode;
+        Debug.Log("Insert " + node.ToString() + " at " + i + " success!");
+        return true;
+    }
+
+    public void DisplayList()
+    {
+        StringBuilder sb = new StringBuilder();
+        Node node = head.next;
+        while(node != null)
+        {
+            sb.Append(node.value.ToString());
+            sb.Append(",");
+            node = node.next;
+        }
+
+        Debug.LogError(sb.ToString());
+    }
+
+}
