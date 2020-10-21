ac1a6e13fb8981e7ddea78e1cf4c1316f0eca2cb
VitensXX <vitens@foxmail.com>
2020-10-21 14:17:58 +0800

    BFS

--- Assets/Scripts/TestPractice/TestForPractice.cs
+++ Assets/Scripts/TestPractice/TestForPractice.cs
@@ -17,16 +17,20 @@ public class TestForPractice : MonoBehaviour
 
     private void Start()
     {
-        Node head = CreateList(new int[] { 4, 6, 9, 11 }, true);
-        Node head2 = CreateList(new int[] { 1, 3, 6, 8, 12 }, true);
+        //Node head = CreateList(new int[] { 4, 6, 9, 11 }, true);
+        //Node head2 = CreateList(new int[] { 1, 3, 6, 8, 12 }, true);
 
-        //Debug.LogError(GetLength(null)+" "+GetLength2(null));
-        //Debug.LogError(GetLength(head) +" "+GetLength2(head));
+        ////Debug.LogError(GetLength(null)+" "+GetLength2(null));
+        ////Debug.LogError(GetLength(head) +" "+GetLength2(head));
 
-        //Node n = Fanzhuan(head);
-        //Log(n);
+        ////Node n = Fanzhuan(head);
+        ////Log(n);
 
-        Log(Merge(head, head2));
+        //Log(Merge(head, head2));
+
+        //Debug.Log(GetMinHeight());
+
+        Debug.Log(Mima("0202"));
     }
 
     Node CreateList(int[] arr, bool log = false)
@@ -220,5 +224,160 @@ public class TestForPractice : MonoBehaviour
 
         return "";
     }
+    #endregion
+
+    #region BFS 双向BFS优化
+
+    #region 二叉树的最小高度
+    /*
+     * [3,9,20,null,null,15,7]
+     */
+    class TNode
+    {
+        public TNode L;
+        public TNode R;
+        public int val;
+
+        public TNode(int v)
+        {
+            val = v;
+        }
+    }
+
+    // 二叉树的最小高度
+    public static int GetMinHeight()
+    {
+        //构造二叉树
+        TNode node9 = new TNode(9);
+        TNode node15 = new TNode(15);
+        TNode node7 = new TNode(7);
+        TNode node20 = new TNode(20);
+        TNode root = new TNode(3);
+        root.L = node9;
+        root.R = node20;
+        node20.L = node15;
+        node20.R = node7;
+
+        //开始
+        Queue<TNode> q = new Queue<TNode>();
+        q.Enqueue(root);
+        int step = 1;
+        while(q.Count > 0)
+        {
+            int size = q.Count;
+            for (int i = 0; i < size; i++)
+            {
+                TNode cur = q.Dequeue();
+                if(cur.L == null && cur.R == null)
+                {
+                    return step;
+                }
+
+                //找到当前节点的左右子节点并添加进queue
+                if(cur.L != null)
+                {
+                    q.Enqueue(cur.L);
+                }
+                if(cur.R != null)
+                {
+                    q.Enqueue(cur.R);
+                }
+            }
+            step++;
+        }
+
+        return step;
+    }
+    #endregion
+
+    #region 密码锁
+
+    //i的位置加1
+    string PlusOne(string s, int i)
+    {
+        char[] arr = s.ToCharArray();
+        if(arr[i] == '9')
+        {
+            arr[i] = '0';
+        }
+        else
+        {
+            arr[i]++;
+        }
+
+        return new string(arr);
+    }
+    //i的位置减1
+    string MinusOne(string s, int i)
+    {
+        char[] arr = s.ToCharArray();
+        if (arr[i] == '0')
+        {
+            arr[i] = '9';
+        }
+        else
+        {
+            arr[i]--;
+        }
+
+        return new string(arr);
+    }
+
+    int Mima(string target)
+    {
+        List<string> deadends = new List<string>() { "0201", "0101", "0102", "1212", "2002" };
+        Queue<string> q = new Queue<string>();
+        List<string> visited = new List<string>();
+        q.Enqueue("0000");
+        visited.Add("0000");
+        int step = 0;
+        while(q.Count > 0)
+        {
+            int size = q.Count;
+            for (int i = 0; i < size; i++)
+            {
+                string cur = q.Dequeue();
+
+                //遇到不能用的直接继续
+                if (deadends.Contains(cur))
+                {
+                    continue;
+                }
+                //达成目标
+                if(cur == target)
+                {
+                    return step;
+                }
+                else
+                {
+                    //将该位置后面的可能性(八个)加入
+                    for (int j = 0; j < 4; j++)
+                    {
+                        string plus = PlusOne(cur, j);
+                        string minus = MinusOne(cur, j);
+
+                        //已经访问过的 不能走回头路
+                        if (!visited.Contains(plus))
+                        {
+                            q.Enqueue(plus);
+                            visited.Add(plus);
+                        }
+                        if (!visited.Contains(minus))
+                        {
+                            q.Enqueue(minus);
+                            visited.Add(minus);
+                        }
+                    }
+                }
+            }
+            step++;
+        }
+
+        return -1;
+    }
+
+
+    #endregion
+
     #endregion
 }
